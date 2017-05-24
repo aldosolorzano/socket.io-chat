33cde86cebcbd0f31f617a97dbbd3356078bc728
aldosolorzano <aldosolorzanop@gmail.com>
2017-05-24 16:56:36 -0500

    showing online users and when someone is typing, with minor bugs to fix

--- public/main.js
+++ public/main.js
@@ -1,6 +1,9 @@
 $(function () {
   const userName = prompt("Choose your nickname");
   const socket = io();
+
+  socket.emit('add user',userName);
+
   $('form').submit(function(){
     let message = $('#m').val();
     socket.emit('chat message', {
@@ -11,7 +14,21 @@ $(function () {
     $('#m').val('');
     return false;
   });
+
+  $('#m').focusin(function(){
+    console.log('focus');
+    socket.emit('typing',userName)
+  })
   socket.on('chat message', function(client){
     $('#messages').append(`<li><strong>${client.userName}: </strong>${client.message}</li>`);
   });
+
+  socket.on('add user',function(onlineUsers){
+    $('#onlineUsers').text(onlineUsers)
+  });
+
+  socket.on('typing',function(userName){
+    $('#userTyping').empty().fadeIn('fast').append(userName + ' is typing...').fadeOut(3000)
+  });
+
 });
